8fd2de1913529b1936f3ceb8bf6a51ca9d8510d5
kimdo <kimdo@DESKTOP-8HEIIH8>
2020-03-24 12:03:36 +0900

    ...

--- Laundry/src/main/java/com/example/web/Filter/IEFilter.java
+++ Laundry/src/main/java/com/example/web/Filter/IEFilter.java
@@ -30,6 +30,10 @@ public class IEFilter implements Filter {
 		// TODO Auto-generated method stub
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
+	    request.setCharacterEncoding("UTF-8");
+		response.setHeader("Expires ", "0s");
+		response.setHeader("Cache-Control", "no-store, no-cache, max-age=0, post-check=0, pre-check=0, public, must-revalidate, proxy-revalidate");
+		response.setHeader("Pragma", "no-cache");
 		response.setHeader("Content-Type", "text/html;charset=EUC-KR");
 		response.setHeader("X-UA-Compatible", "IE=Edge,chrome=1");
 		chain.doFilter(request, response);
